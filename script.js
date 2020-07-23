462b88063871de15da6c6a6324a8389ce80bb43d
Дорошенко Алина <belyanina.07@mail.ru>
2020-07-23 12:22:45 +0300

    new changes in index, style, script

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
+//3. клик на простой уровень и добавление активного уровня
+
 const simpleLevel = document.getElementById('simple');
 console.log(simpleLevel);
 
-const difficultyLevel = () => {
-	simpleLevel.classList.toggle('active-level');
+const difficultyLevel = (btn) => {
+	simpleLevel.classList.remove('active-level');
+	btn.target.classList.add('active-level');
 	console.log('Ура!')
 };
+
 simpleLevel.addEventListener('click', difficultyLevel);
 
-// const levels = document.querySelectorAll('input.game__simple, input.game__medium, input.game__hard');
+
+
+// клики на все уровни и переключение активного уровня с квадратиком
+
+// const levels = document.querySelectorAll('.game-level');
 // console.log(levels);
 
-// const difficultyLevel = () => {
-// 	levels.classList.toggle('active-level');
+// const difficultyLevel = (btn) => {
+// 	levels.forEach(btn => btn.classList.remove('active-level'));
+// 	btn.target.classList.add('active-level');
 // 	console.log('Ура!');
 // };
 
-// levels.forEach(inp => {
-// 	inp.addEventListener('click', difficultyLevel);
+// levels.forEach(btn => {
+// 	btn.addEventListener('click', difficultyLevel);
+// 	// btn.classList.toggle('active-level');
 // });
+
+
+
+// 4. 
+
+
+//5. клик на кнопку Начать игру, но здесь еще нужна функция, которая запустит игру
+
+// const start = document.getElementById('begin');
+// console.log(start);
+
+// const beginGame = () => {
+// 	console.log('Поехали');
+// };
+
+// start.addEventListener('click', beginGame);
